215a3317dbc145170367b07137b126cd9a63cf3c
Aanaelle <anaellemimi27@gmail.com>
2022-10-09 16:06:59 +0200

    levelSprite

--- levels/LevelManager.java
+++ levels/LevelManager.java
@@ -9,23 +9,31 @@ import utilz.LoadSave;
 public class LevelManager {
 
 	private Game game;
-	private BufferedImage levelSprite;
+	private BufferedImage[] levelSprite;
 	
 	public LevelManager(Game game)
 	{
 		this.game = game;
-		this.levelSprite = LoadSave.getSpriteAtlas(LoadSave.LEVEL_ATLAS);
+		//this.levelSprite = LoadSave.getSpriteAtlas(LoadSave.LEVEL_ATLAS);
 		importOutsideSprites();
 	}
 	
-	private void importOutsideSprites() {
-		//levelSprite = new BufferedImage[];
+	private void importOutsideSprites() 
+	{
+		BufferedImage img = LoadSave.getSpriteAtlas(LoadSave.LEVEL_ATLAS);
 		
+		levelSprite = new BufferedImage[48];
+		for(int j = 0; j < 4; j++) //hauteur de l'image
+			for(int i = 0; i < 12; i++) //largeur de l'image
+			{
+				int index = j*12 + i;
+				this.levelSprite[index] = img.getSubimage(i*32, j*32, 32, 32);
+			}
 	}
 
 	public void draw(Graphics g)
 	{
-		g.drawImage(levelSprite, 0, 0, null);
+		g.drawImage(levelSprite[2], 0, 0, null);
 	}
 	
 	public void update()
